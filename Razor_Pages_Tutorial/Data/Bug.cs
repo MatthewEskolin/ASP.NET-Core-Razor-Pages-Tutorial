d5d34a491c0f5c51245c34cd7b0d6c464f40934c
Matthew Eskolin <matteskolin@gmail.com>
2018-11-09 12:06:04 -0600

    check in before scaffold

---
+++ Razor_Pages_Tutorial/Data/Bug.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_Pages_Tutorial.Data
+{
+    public class Bug
+    {
+        public int BugID { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Title { get; set; }
+
+        public BugSeverity Severity { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+
+    }
+
+
+    public enum BugSeverity
+    {
+        Undefined = 0,
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+
+    
+
+
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int BugID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int UserId { get; set; }
+
+        public Bug Bug { get; set; }
+
+    }
+
+
+       
+}
